59c40f6a5c3a1a501ec2d89dbb7def1386974cc0
Falon Darville <falondarville@gmail.com>
2018-02-28 13:42:28 -0800

    add starter links

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+var config = {
+	apiKey: "AIzaSyAWzaJ9XRdN3LDeK4sCii2LzP7BHZlu88Q",
+	authDomain: "trainschedule-fcff2.firebaseapp.com",
+	databaseURL: "https://trainschedule-fcff2.firebaseio.com",
+	projectId: "trainschedule-fcff2",
+	storageBucket: "",
+	messagingSenderId: "819990671964"
+};
+
+firebase.initializeApp(config);
+
+});
